a8da9168a4cff17032b47f755fdc5d00ad627b8d
Bonnefoy Kévin <93328879+dzagoev3407@users.noreply.github.com>
2024-05-12 00:08:31 +0200

    Add files via upload

--- includes/navMenu/homeClient.php
+++ includes/navMenu/homeClient.php
@@ -9,8 +9,8 @@
       <a class="dropdown-item" href="#" data-toggle="modal" data-target="#modalAllRdv">Voir mes rdv</a>
     </div>
   </div>
-    <li class="nav-link"><a class="text-dark text-decoration-none" href="#">Mon profil</a></li>
-    <li class="nav-link"><a class="text-dark text-decoration-none" href="#">Mon suivi</a></li>
+    <li class="nav-link"><a class="text-dark text-decoration-none" href="profil/profil.php">Mon profil</a></li>
+    <li class="nav-link"><a class="text-dark text-decoration-none" href="profil/suivi.php">Mon suivi</a></li>
     <li class="nav-link"><a class="text-dark text-decoration-none" href="logout.php"><button class="btn btn-danger">Déconnexion</button></a></li>
   </ul>
 </div>
